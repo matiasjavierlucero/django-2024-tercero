2d90f93c75ceec59b6345f150aaece740081b99a
matiasjavierlucero <matiasjavierlucero@gmail.com>
2024-06-07 02:13:17 +0000

    settings

--- miniblog/miniblog/settings.py
+++ miniblog/miniblog/settings.py
@@ -66,7 +66,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
-                # SELF CONTEXTS
+                # MIS CONTEXTOS
                 "miniblog.context_processors.all_names_product",
                 "miniblog.context_processors.all_names_category"
             ],
